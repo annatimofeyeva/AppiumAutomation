fd959d6ac13bd1b1884a7ada246220d2c7f6f2d2
Anna Timofeeva <anjatimofeyeva@gmail.com>
2018-11-24 18:55:32 -0800

    project structure created, libs are connected to the project, first Test passed

---
+++ src/FirstTest.java
@@ -0,0 +1,42 @@
+import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.android.AndroidDriver;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import java.net.URL;
+
+public class FirstTest {
+
+    private AppiumDriver driver;
+
+    @Before
+    public void setUp() throws  Exception
+    {
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability("platformName","Android");
+        capabilities.setCapability("deviceName","AndroidTestDevice");
+        capabilities.setCapability("platformVersion","7.1.1");
+        capabilities.setCapability("automationName","Appium");
+        capabilities.setCapability("appPackage","org.wikipedia");
+        capabilities.setCapability("appActivity",".main.MainActivity");
+        capabilities.setCapability("app","/home/anya/AppiumAutomation/apks/org.wikipedia.apk");
+
+        // sent to Androiddriver all capabilities and where it should start
+
+        driver = new AndroidDriver(new URL("http://127.0.0.1:4723/wd/hub"), capabilities);
+    }
+
+    @After
+    public void tearDown()
+    {
+        driver.quit();
+    }
+
+    @Test
+    public void firstTest()
+    {
+        System.out.println("Run first test!");
+    }
+}
