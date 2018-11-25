52a4b48182f0a6854f132ca62a2ec7ad0036b618
Anna Timofeeva <anjatimofeyeva@gmail.com>
2018-11-25 14:22:05 -0800

    first WIKI test passed

--- src/FirstTest.java
+++ src/FirstTest.java
@@ -3,9 +3,10 @@ import io.appium.java_client.android.AndroidDriver;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.remote.DesiredCapabilities;
-
 import java.net.URL;
+
 
 public class FirstTest {
 
@@ -17,13 +18,11 @@ public class FirstTest {
         DesiredCapabilities capabilities = new DesiredCapabilities();
         capabilities.setCapability("platformName","Android");
         capabilities.setCapability("deviceName","AndroidTestDevice");
-        capabilities.setCapability("platformVersion","7.1.1");
+        capabilities.setCapability("platformVersion","6.0");
         capabilities.setCapability("automationName","Appium");
         capabilities.setCapability("appPackage","org.wikipedia");
         capabilities.setCapability("appActivity",".main.MainActivity");
         capabilities.setCapability("app","/home/anya/AppiumAutomation/apks/org.wikipedia.apk");
-
-        // sent to Androiddriver all capabilities and where it should start
 
         driver = new AndroidDriver(new URL("http://127.0.0.1:4723/wd/hub"), capabilities);
     }
@@ -37,6 +36,9 @@ public class FirstTest {
     @Test
     public void firstTest()
     {
-        System.out.println("Run first test!");
+        //System.out.println("Run first test!");
+
+        WebElement element = driver.findElementByXPath("//*[contains(@text,'Search Wikipedia')]");
+        element.click();
     }
 }
