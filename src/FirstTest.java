3a862f07c1fb7fc96023ad3e9371214abf01d47a
Anna Timofeeva <anjatimofeyeva@gmail.com>
2018-11-26 23:08:22 -0800

    testSearchAndDeleteSearchResults() added

--- src/FirstTest.java
+++ src/FirstTest.java
@@ -10,7 +10,9 @@ import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
+
 import java.net.URL;
+import java.util.List;
 
 
 public class FirstTest {
@@ -31,10 +33,10 @@ public class FirstTest {
         driver = new AndroidDriver(new URL("http://127.0.0.1:4723/wd/hub"), capabilities);
     }
 
-    @After
-    public void tearDown() {
-        driver.quit();
-    }
+//    @After
+//    public void tearDown() {
+//        driver.quit();
+//    }
 
     @Test
     public void firstTest() {
@@ -96,6 +98,7 @@ public class FirstTest {
 
     @Test
     public void testCompareArticleTitle() {
+
         waitForElementAndClick(
                 By.xpath("//*[contains(@text,'Search Wikipedia')]"),
                 "Cannot find element to init search",
@@ -129,8 +132,7 @@ public class FirstTest {
     }
 
     @Test
-    public void testSearchForWordSearchInSearchInputField()
-    {
+    public void testSearchForPromptWordInSearchInputField() {
         waitForElementAndClick(
                 By.id("org.wikipedia:id/search_container"),
                 "Cannot find 'Search Wikipedia' in input field",
@@ -150,6 +152,173 @@ public class FirstTest {
                 search_prompt_text
         );
     }
+
+    @Test
+    public void testSearchAndDeleteSearchResults() {
+
+        waitForElementAndClick(
+                By.xpath("//*[contains(@text,'Search Wikipedia')]"),
+                "Cannot find element to init search",
+                5
+        );
+
+        waitForElementAndSendKeys(
+                By.id("org.wikipedia:id/search_src_text"),
+                "Seattle",
+                "Cannot find search input",
+                5
+        );
+
+
+        String search_result_locator = "//*[@class='android.widget.LinearLayout']/*[@resource-id='org.wikipedia:id/page_list_item_image']";
+        String search_line = "Seattle";
+
+
+        waitForElementPresent(By.xpath(search_result_locator),
+                "Cannot find anything by the request" + search_line,
+                15
+        );
+
+        int amount = getAmountOfWebElements(By.xpath(search_result_locator));
+        System.out.println(amount);
+
+        Assert.assertTrue("Number of articles more then 1", amount > 1);
+
+        waitForElementAndClick(
+                By.id("org.wikipedia:id/search_close_btn"),
+                "Cannot find X button",
+                15
+        );
+
+        WebElement screen_element = waitForElementPresent(
+                By.id("org.wikipedia:id/search_empty_message"),
+                "Cannot find search message",
+                15
+        );
+
+        // variable with search message text
+
+        String search_message_text_in_the_middle_of_the_page = screen_element.getAttribute("text");
+
+        // assert for search message text
+
+        Assert.assertEquals(
+                "Unexpected search message displays",
+                "Search and read the free encyclopedia in your language",
+                search_message_text_in_the_middle_of_the_page
+        );
+
+        // printing search message text to console
+
+        System.out.println(search_message_text_in_the_middle_of_the_page);
+
+
+        WebElement search_input_field_element = waitForElementPresent(
+                By.id("org.wikipedia:id/search_src_text"),
+                " is not found",
+                15
+        );
+
+        String search_prompt_text = search_input_field_element.getAttribute("text");
+        System.out.println(search_prompt_text);
+
+        Assert.assertEquals(
+                "Unexpected prompt text in search input field ",
+                "Search…",
+                search_prompt_text
+        );
+
+        waitForElementAndSendKeys(
+                By.id("org.wikipedia:id/search_src_text"),
+                "Java",
+                "Cannot find search input",
+                10
+        );
+
+        String input_text_string = search_input_field_element.getAttribute("text");
+        System.out.println(input_text_string);
+
+        Assert.assertEquals("Java", input_text_string
+        );
+    }
+
+    //List<WebElement> allArticles = driver.findElementsByXPath("//*[@class='android.widget.LinearLayout']/*[@resource-id='org.wikipedia:id/page_list_item_image']");
+
+    //findElementsByXPath("//*[@class='android.widget.LinearLayout']/*[@resource-id='org.wikipedia:id/page_list_item_image']");
+
+
+//        int n = allArticles.size();
+//        System.out.println("Number of all articles " + n);
+//        for (int i = 0; i < n; i++) {
+//            allArticles.get(i).click();
+//        }
+
+
+//        for (WebElement webElement : allArticles) {
+//            System.out.println(webElement.getSize());
+//            System.out.println(webElement.getTagName());
+//        }
+
+
+    // create variable with the locator:
+    //String article_search_result_locator =
+
+
+//
+//    @Test
+//    public void testAmountOfNotEmptySearch()
+//    {
+//        waitForElementAndClick(
+//                By.xpath("//*[contains(@text,'Search Wikipedia')]"),
+//                "Cannot find 'Search Wikipedia' input.",
+//                5
+//        );
+//
+//        String search_line = "Linkin Park Diskography"; // we need it to reuse now and to make it a dataProvider param in future
+//        waitForElementAndSendKeys(
+//                By.xpath("//*[contains(@text,'Search…')]"),
+//                search_line,
+//                "Cannot find search input",
+//                5
+//        );
+//
+//        String search_result_locator = "//*[@resource-id='org.wikipedia:id/search_results_list']/*[@resource-id='org.wikipedia:id/page_list_item_container']";
+//        waitForElementPresent(
+//                By.xpath(search_result_locator),
+//                "Cannot find anything by the request " + search_line,
+//                15
+//        );
+//
+//        int amount_of_search_results = getAmountOfElements(
+//                By.xpath(search_result_locator)
+//        );
+//
+//
+//        /*
+//         * А вот нам и понадобился hamcrest-либа
+//         * Знакомься, это аналог assertGreaterThan :)
+//         * Я считаю, это надо показать в курсе и рассказать об этом.
+//         * Но смотри сам - ниже есть просто assertTrue.
+//         * */
+//        Assert.assertThat(
+//                "We found too few results!",
+//                amount_of_search_results,
+//                //Matchers.greaterThan(50) // to fail
+//                Matchers.greaterThan(0) // to pass
+//        );
+//
+//        /*Assert.assertTrue(
+//                "We found too few results!",
+//                amount_of_search_results > 0
+//        );*/
+//    }
+
+    /**
+     * We need this one and testAmountOfNotEmptySearch to show how "find elements" method works
+     * Also we'll use assertGreaterThen and assertLessThen functions
+     * Also we'll refactor this two tests in one with dataProvider
+     */
+
 
     private WebElement waitForElementPresent(By by, String error_message, long timeoutInSeconds) {
 
@@ -181,9 +350,17 @@ public class FirstTest {
         );
     }
 
+
     private WebElement waitForElementAndClear(By by, String error_message, long timeoutInSeconds) {
         WebElement element = waitForElementPresent(by, error_message, timeoutInSeconds);
         element.clear();
         return element;
     }
+
+    private int getAmountOfWebElements(By by) {
+        List elements = driver.findElements(by);
+        int a = elements.size();
+        return a;
+    }
+
 }
