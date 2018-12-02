c4394227cf46f1755011d1636fffdb2368e58fc8
Anna Timofeeva <anjatimofeyeva@gmail.com>
2018-12-01 23:08:29 -0800

    code refactoring: CoreTestCase.java added

--- src/WikiTestClass.java
+++ src/WikiTestClass.java
@@ -1,45 +1,24 @@
 import io.appium.java_client.AppiumDriver;
 import io.appium.java_client.TouchAction;
-import io.appium.java_client.android.AndroidDriver;
-import org.junit.After;
+import lib.CoreTestCase;
 import org.junit.Assert;
-import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Dimension;
 import org.openqa.selenium.ScreenOrientation;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
-import java.net.URL;
 import java.util.List;
 
-public class WikiTestClass {
+public class WikiTestClass extends CoreTestCase {
 
-    private AppiumDriver driver;
 
-    @Before
-    public void setUp() throws Exception {
-        DesiredCapabilities capabilities = new DesiredCapabilities();
-        capabilities.setCapability("platformName", "Android");
-        capabilities.setCapability("deviceName", "AndroidTestDevice");
-        capabilities.setCapability("platformVersion", "6.0");
-        capabilities.setCapability("automationName", "Appium");
-        capabilities.setCapability("appPackage", "org.wikipedia");
-        capabilities.setCapability("appActivity", ".main.MainActivity");
-        capabilities.setCapability("app", "/home/anya/AppiumAutomation/apks/org.wikipedia.apk");
 
-        driver = new AndroidDriver(new URL("http://127.0.0.1:4723/wd/hub"), capabilities);
-    }
-    @After
-    public void tearDown() {
-        driver.rotate(ScreenOrientation.PORTRAIT);
-        driver.quit();
-    }
-//  -----print "Java" in Wiki search field and waitForElementPresent
+
+//  -----testSearch name is for the JUnit; All test need to start with "test". JUnit is now able to see this test and start it
     @Test
-    public void firstTest() {
+    public void testSearch() {
         waitForElementAndClick(
                 By.xpath("//*[contains(@text,'Search Wikipedia')]"),
                 "Cannot find element to init search",
@@ -171,10 +150,6 @@ public class WikiTestClass {
                 "Cannot find X button",
                 15
         );
-        assertElementNotPresent(
-                By.xpath(search_result_locator),
-                "We still can find some articles on page after deleting"
-        );
     }
 //  -----just for practice: different assertions:
 
@@ -277,7 +252,7 @@ public class WikiTestClass {
     }
 //  -------- save article to list, then delete from list by swipe, then verification - waitForElementNotPresent()
     @Test
-    public void saveFirstArticleToMyListAndDeleteBySwipe() {
+    public void testSaveFirstArticleToMyListAndDeleteBySwipe() {
         waitForElementAndClick(
                 By.xpath("//*[contains(@text,'Search Wikipedia')]"),
                 "Cannot find element to init search",
@@ -713,10 +688,10 @@ public class WikiTestClass {
                 "Cannot find 'Object-oriented programming language' topic searching by ",
                 5
         );
-        assertElementPresent(
-                By.id("org.wikipedia:id/view_page_title_text"),
-                "Cannot find article title immediately "
-        );
+//        assertElementPresent(
+//                By.id("org.wikipedia:id/view_page_title_text"),
+//                "Cannot find article title immediately "
+//        );
     }
 
 
